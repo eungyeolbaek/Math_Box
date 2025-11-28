3cadbbdfb55779d0f0a61ec1af0206f176e74398
eungyeolbaek <eungyeolbaek@gmail.com>
2025-11-28 18:33:58 +0900

    Add multiply function
    calculator.js supports multiply now

--- calculator.js
+++ calculator.js
@@ -1,7 +1,14 @@
+// (x: number, y:number) -> number
 function add(a, b) {
     return a + b;
 }
 
+// (x: number, y:number) -> number
 function substract(a, b) {
     return a - b;
+}
+
+// (x: number, y:number) -> number
+function multiply(a, b) {
+    return a * b;
 }
